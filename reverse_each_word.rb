5c3e8939ec232f59fd06967cefa14f02db97d833
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-12 18:05:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,7 @@ def each_reverse_each_word(sentence)
   reversed_sentence.chomp(" ")
 end
 
-def each_reverse_each_word(sentence)
+def reverse_each_word(sentence)
   reversed_sentence = ""
   word_array = sentence.split(" ")
   word_array.collect do | word |
